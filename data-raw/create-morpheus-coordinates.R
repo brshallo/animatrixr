a9d10f175f8fb37b634da90f63a6f29e538c218a
brshallo <brshallo@gmail.com>
2020-10-01 10:33:19 -0400

    add comment on where code for image creation was learned from

--- data-raw/create-morpheus-coordinates.R
+++ data-raw/create-morpheus-coordinates.R
@@ -3,6 +3,8 @@ library(magick)
 # https://i.pinimg.com/originals/b1/22/ba/b122ba1de60f1b04b7a93e1597f688a7.jpg
 matrix_spoon <- magick::image_read(here::here("inst", "extdata", "morpheus3-2.jpg"))
 
+# Code for this is mostly copied from:
+# https://github.com/ropensci/magick/issues/101#issuecomment-353191334
 bitmap <- matrix_spoon %>%
   image_edge(radius = 1.5) %>%
   image_convert(type = 'grayscale') %>%
